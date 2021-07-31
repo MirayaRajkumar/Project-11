450735163dd546b2e9a22f59b5388d14c714bdcb
MirayaRajkumar <87897710+MirayaRajkumar@users.noreply.github.com>
2021-07-31 12:23:50 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,3 +1,7 @@
+var path, person;
+var backgroundImg, person_running;
+var edges;
+
 function preload(){
   //pre-load images
 backgroundImg = loadAnimation("path.png")
@@ -8,16 +12,31 @@ function setup(){
   createCanvas(600,600);
   background(30);
   //create sprites here
-  var path = createSprite(300,300);
+  path = createSprite(300,300);
   path.addAnimation("path",backgroundImg);
   path.velocityY = 4;
   path.scale = 1.1;
-  var person = createSprite(300,300);
+  person = createSprite(300,300);
   person.addAnimation("running",person_running);
   person.scale = 0.1;
+  edges = createEdgeSprites();
 }
 
 function draw() {
   background(0);
+  if(path.y > 500){
+    path.y = 300;
+  }
+  if(keyIsDown(RIGHT_ARROW)){
+    person.x = person.x + 2;
+  }
+  if(keyIsDown(LEFT_ARROW)){
+    person.x = person.x + -2;
+  }
+  
+  person.bounceOff(edges[0]);
+  person.bounceOff(edges[1]);
+  person.collide(edges[3]);
   drawSprites();
 }
+
